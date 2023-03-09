6d5bb0666fd29c814526c71b3e73ee27c31d140e
amer200 <amermostaafa@gmail.com>
2023-03-09 10:34:37 +0200

    commit

--- routes/supplier.js
+++ routes/supplier.js
@@ -4,5 +4,4 @@ const supplierControllers = require("../controllers/supplier");
 const supplierMiddlewares = require('../middlewares/supplier');
 routes.post('/signup', supplierMiddlewares.isValide, supplierControllers.signUp);
 routes.post('/login', supplierControllers.logIn);
-routes.get()
 module.exports = routes;
