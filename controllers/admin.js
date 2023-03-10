4bffccedba7c3e77443ba1e34d5c1d2734032ed9
amer200 <amermostaafa@gmail.com>
2023-03-10 03:09:53 +0200

    commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,19 @@
 const Categ = require("../models/category");
+const jwt = require("jsonwebtoken");
+
 exports.logIn = (req, res) => {
     const password = req.body.password;
-    console.log(process.env.ADMINPASS)
     if (password == process.env.ADMINPASS) {
         req.session.admin = true;
+        const user = {
+            id: "123",
+            name: "admin",
+            roll: "admin"
+        }
+        const token = jwt.sign({ user: user }, process.env.ACCESS_TOKEN);
         res.status(200).json({
-            msg: "ok"
+            msg: "ok",
+            token: token
         })
     } else {
         res.status(304).json({
