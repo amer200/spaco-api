845c46afd1d8b4ace9cd6aedf9d919942257f4fe
amer200 <amermostaafa@gmail.com>
2023-03-16 15:29:22 +0200

    commit

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 app.post('/admin/add-new-category', upload.single('img'));
 app.post("/products/add-new", upload.array('imgs'));
+app.post("/products/edit-prod/:pid", upload.array('imgs'));
 /********************************************************************************** */
 const store = new MongoDBStore({
     uri: dbUrl,
--- routes/product.js
+++ routes/product.js
@@ -9,5 +9,6 @@ routes.get('/product-by-id/:id', prodController.getProdById);
 routes.get('/product-by-category/:name', prodController.getProdsByCateg);
 routes.post('/add-new', supplierMiddlewares.isAuth, categMiddlewares.isFind, prodMiddlewares.isValide, prodController.AddProd);
 routes.get('/categs', prodController.getCategs);
-
+routes.post('/edit-prod/:pid', supplierMiddlewares.isAuth, categMiddlewares.isFind, prodMiddlewares.isValide, prodController.EditProd);
+routes.post('/remove-img/:pid', supplierMiddlewares.isAuth, prodController.removeImg);
 module.exports = routes
